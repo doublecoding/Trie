971c2aaef922781781aa160957ed60efd722bf6b
doublecoding <44154442+doublecoding@users.noreply.github.com>
2020-06-22 22:09:48 +0500

    Adding Project

---
+++ TrieStruct/Node.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace TrieStruct
+{
+    public class Node<T>
+    {
+        public T Key { get; set; }
+        public string Word { get; set; }
+        public bool IsWord { get; set; }
+        public Dictionary<char, Node<T>> Children { get; set; }
+        public int CountChildren { get { return Children.Count; } }
+
+        public Node(T key, bool isWord)
+        {
+            Key = key;
+            Word = "";
+            IsWord = isWord;
+            Children = new Dictionary<char, Node<T>>();
+        }
+
+        public bool FindChild(char key) => (Children.ContainsKey(key) ? true : false);
+
+        public Node<T> GetChild(char key) => (Children.ContainsKey(key) ? Children[key] : this);
+
+        public Node<T> AddChild(char key, bool isWord, T data)
+        {
+            Node<T> child = new Node<T>(data, isWord);
+            Children.Add(key, child);
+            return child;
+        }
+    }
+}
---
+++ TrieStruct/Trie.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections.Generic;
+
+namespace TrieStruct
+{
+    public class Trie<T>
+    {
+        private Node<T> root = new Node<T>(default(T), false);
+
+        public void AddWord(string word, T data)
+        {
+            var currentNode = root;
+            for (var i = 0; i < word.Length; i++)
+            {
+                if (!currentNode.FindChild(word[i]))
+                {
+                    if (i != word.Length - 1)
+                        currentNode.AddChild(word[i], false, default(T));
+                    else
+                    {
+                        currentNode.AddChild(word[i], true, data);
+                        currentNode = currentNode.GetChild(word[i]);
+                        currentNode.Word = word;
+                    }
+                }
+                else if (i == word.Length - 1)
+                {
+                    currentNode = currentNode.GetChild(word[i]);
+                    currentNode.Key = data;
+                    currentNode.IsWord = true;
+                    currentNode.Word = word;
+                    break;
+                }
+                currentNode = currentNode.GetChild(word[i]);
+            }
+        }
+        public bool DeleteWord(string word)
+        {
+            var currentNode = root;
+            foreach (char symbol in word)
+            {
+                if (!currentNode.FindChild(symbol))
+                    return false;
+                currentNode = currentNode.GetChild(symbol);
+            }
+            currentNode.IsWord = false;
+            currentNode.Key = default(T);
+            currentNode.Word = "";
+            return true;
+        }
+
+        public T GetDataOfWord(string word)
+        {
+            var currentNode = root;
+            foreach (char symbol in word)
+            {
+                if (!currentNode.FindChild(symbol))
+                    return default(T);
+                currentNode = currentNode.GetChild(symbol);
+            }
+            if (currentNode.IsWord == true)
+                return currentNode.Key;
+            return default(T);
+        }
+
+        public List<string> GetWordsForPrefix(string prefix)
+        {
+            var wordsForPrefix = new List<string>();
+            var currentNode = root;
+            foreach (var symbol in prefix)
+            {
+                if (currentNode.FindChild(symbol))
+                    currentNode = currentNode.GetChild(symbol);
+                else
+                    return wordsForPrefix;
+            }
+            FindAllChildrenWords(currentNode, wordsForPrefix);
+            return wordsForPrefix;
+        }
+
+        private void FindAllChildrenWords(Node<T> currentNode, List<string> wordsForPrefix)
+        {
+            if (currentNode.IsWord)
+                wordsForPrefix.Add(currentNode.Word);
+            foreach (var node in currentNode.Children.Values)
+                FindAllChildrenWords(node, wordsForPrefix);
+        }
+    }
+}
